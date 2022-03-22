e098ea35260e9470ad396610ec9df0a9a79fbe78
LinuxArmy <51884521+MrFploit@users.noreply.github.com>
2022-03-22 21:25:36 +0430

    Update packages.py

--- packages.py
+++ packages.py
@@ -1,10 +1,7 @@
+#LinuxArmy
+#MrFploit
 import os
-
 packages = ['pytelegrambotapi']
-
 os.system(f'pip3 uninstall telebot -y')
 os.system(f'pip3 uninstall pytelegrambotapi -y')
-
 os.system('pip install -U git+https://github.com/eternnoir/pyTelegramBotAPI.git')
-#for package in packages:
-#  os.system(f'pip3 install {package}')
